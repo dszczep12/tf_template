0a871a3a0508bc93f4ce3ef0eabe0453e9a1e1d8
dszczep12 <adorevi9@gmail.com>
2024-07-31 14:47:00 -0500

    Update generate_tf.py

--- root/generate_tf.py
+++ root/generate_tf.py
@@ -1,22 +1,20 @@
 import yaml
 from jinja2 import Environment, FileSystemLoader
 
-# Load the YAML configuration file
 with open('config.yaml', 'r') as file:
     config = yaml.safe_load(file)
 
-# Load the Jinja2 template files
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('main.tf.jinja')
 variables_template = env.get_template('variables.tf.jinja')
 outputs_template = env.get_template('outputs.tf.jinja')
 
-# Render the templates with the config variables
+# Rendering
 rendered_main_tf = template.render(config)
 rendered_variables_tf = variables_template.render(config)
 rendered_outputs_tf = outputs_template.render(config)
 
-# Write the rendered templates to Terraform files
+# Writing to file
 with open('terraform/main.tf', 'w') as f:
     f.write(rendered_main_tf)
 
